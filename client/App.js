97cd9ea6eb73a541e32b8b35f5aae79aa11a1e3c
Michael Fan <mfan20107@gmail.com>
2022-01-04 19:40:36 -0500

    feat: add mouse control

--- client/App.js
+++ client/App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { io } from "socket.io-client";
 
-export default function App() {
+import VirtualMousePad from "./components/VritualMousePad";
+
+const socket = io("http://192.168.0.191:5000");
+
+const App = () => {
+  const [message, setMessage] = useState("Not connected");
+
+  const sendCommand = () => {
+    socket.emit("remote_control_message", "Command from client");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.mouseControlContainer}>
+        <VirtualMousePad socket={socket} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    height: "100%",
+    width: "100%",
+    justifyContent: "flex-end",
+  },
+  mouseControlContainer: {
+    width: "100%",
+    height: "40%",
   },
 });
---
+++ client/components/VritualMousePad.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect, useRef } from "react";
+import {
+  PanResponder,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Pressable,
+} from "react-native";
+
+const VirtualMousePad = ({ socket }) => {
+  const pan = useRef(new Animated.ValueXY({ x: 0, y: 0 }));
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderRelease: (e, gestureState) => {
+        const data = {
+          type: "mouse_move",
+          data: {
+            x: Math.floor(gestureState.dx / 2),
+            y: Math.floor(gestureState.dy / 2),
+          },
+        };
+
+        socket.emit("mouse_move", JSON.stringify(data));
+      },
+    })
+  ).current;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.mouseContainer}>
+        <View style={styles.mousePad} {...panResponder.panHandlers}></View>
+        <View style={styles.scrollContainer}>
+          <Pressable
+            style={styles.mouseButton}
+            onPress={() =>
+              socket.emit(
+                "mouse_scroll",
+                JSON.stringify({
+                  type: "mouse_scroll",
+                  data: { amount: 100 },
+                })
+              )
+            }
+          >
+            <Text style={styles.text}>Up</Text>
+          </Pressable>
+          <Pressable
+            style={styles.scrollButton}
+            onPress={() =>
+              socket.emit(
+                "mouse_scroll",
+                JSON.stringify({
+                  type: "mouse_scroll",
+                  data: { amount: -100 },
+                })
+              )
+            }
+          >
+            <Text style={styles.text}>Down</Text>
+          </Pressable>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={styles.scrollButton}
+          onPress={() =>
+            socket.emit(
+              "mouse_click",
+              JSON.stringify({ type: "mouse_click", data: { click: "left" } })
+            )
+          }
+        >
+          <Text style={styles.text}>Left Click</Text>
+        </Pressable>
+        <Pressable
+          style={styles.mouseButton}
+          onPress={() =>
+            socket.emit(
+              "mouse_click",
+              JSON.stringify({ type: "mouse_click", data: { click: "right" } })
+            )
+          }
+        >
+          <Text style={styles.text}>Right Click</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "100%",
+    justifyContent: "flex-end",
+    paddingBottom: 50,
+  },
+  mousePad: {
+    backgroundColor: "#264653",
+    flexBasis: "80%",
+  },
+  mouseContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+  },
+  scrollContainer: {
+    display: "flex",
+    flexBasis: "20%",
+    height: "100%",
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    flexBasis: "30%",
+  },
+  mouseButton: {
+    flexBasis: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: "#2a9d8f",
+  },
+  scrollButton: {
+    flexBasis: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: "#2a9d8f",
+  },
+  text: {
+    color: "#264653",
+    fontSize: 16,
+  },
+});
+
+export default VirtualMousePad;
